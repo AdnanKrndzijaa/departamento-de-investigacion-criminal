4d8287c6f8105ed719a5a2eabb33d82762f6bedd
Adnan Krndzija <92021913+AdnanKrndzijaa@users.noreply.github.com>
2023-03-06 12:46:04 +0100

    modal sheets for admin part added

--- rest/admin.php
+++ rest/admin.php
@@ -4,10 +4,18 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once 'dao/NewsDao.class.php';
+require_once 'dao/WantedDao.class.php';
+require_once 'dao/ReportsDao.class.php';
+
 require_once '../vendor/autoload.php';
 
-Flight::register('newsDao', 'NewsDao');
+require_once __DIR__.'/routes/NewsRoutes.php';
+require_once __DIR__.'/routes/WantedRoutes.php';
+require_once __DIR__.'/routes/ReportsRoutes.php';
 
+Flight::register('newsDao', 'NewsDao');
+Flight::register('wantedDao', 'WantedDao');
+Flight::register('reportsDao', 'ReportsDao');
 
 
 
