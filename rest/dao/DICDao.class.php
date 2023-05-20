0dd99c6ab75ef03461f9fa2c0eec378d3717e0af
Adnan Krndzija <92021913+AdnanKrndzijaa@users.noreply.github.com>
2023-05-20 19:38:49 +0200

    db update

--- rest/dao/DICDao.class.php
+++ rest/dao/DICDao.class.php
@@ -9,11 +9,11 @@ class DICDao {
 
     public function __construct($table_name) {
         $this->table_name = $table_name;
-        $servername = Config::DB_HOST();
-        $username = Config::DB_USERNAME();
-        $password = Config::DB_PASSWORD();
-        $schema = Config::DB_SCHEME();
-        $port = Config::DB_PORT();
+        $servername = "sql7.freemysqlhosting.net";
+        $username = "sql7619727";
+        $password = "cA7njujdHB";
+        $schema = "sql7619727";
+        $port = "3306";
         $this->conn = new PDO("mysql:host=$servername;dbname=$schema;post=$port", $username, $password);
         // set the PDO error mode to exception
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
